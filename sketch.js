0b1c9948224ab919787da9c716b956de87b93d03
shiyamu <72066407+shiyamu@users.noreply.github.com>
2021-05-31 13:06:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var fixedRect, movingRect;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  object1 = createSprite(400,200,60,30);
+  object1.shapeColor = "blue";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  //collision_alg();
+
+  if(collision_alg(movingRect,fixedRect)){ //if(true) if(false)
+    movingRect.shapeColor = "red";
+    fixedRect.shapeColor = "red";
+  }
+  else {
+    movingRect.shapeColor = "green";
+    fixedRect.shapeColor = "green";
+  }
+  
+  drawSprites();
+}
